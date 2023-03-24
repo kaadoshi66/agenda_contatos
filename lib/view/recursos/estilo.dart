861bb49f0d5b0feb5d8f8e5009fd946f86775ca0
kaadoshi66 <igorguilherme00@outlook.com>
2023-03-24 08:49:37 -0300

    upgrade

--- lib/view/recursos/estilo.dart
+++ lib/view/recursos/estilo.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 ThemeData estilo() {
   ThemeData base = ThemeData.dark();
   return base.copyWith(
-    colorScheme: ColorScheme.dark(
-      brightness: Brightness.dark,
-      surface: Colors.lightBlue.shade900,
-      onSurface: Colors.white,
-    ),
+    primaryColor: Colors.redAccent,
+    colorScheme: const ColorScheme.dark(
+        // brightness: Brightness.dark,
+        // surface: Colors.lightBlue.shade900,
+        // onSurface: Colors.white,
+        ),
     floatingActionButtonTheme: FloatingActionButtonThemeData(
       backgroundColor: Colors.lightBlueAccent[300],
     ),
--- lib/view/recursos/menu.dart
+++ lib/view/recursos/menu.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 
 class MenuDrawer extends StatelessWidget {
+  MenuDrawer({super.key});
   //Dados vindo ddo banco de Dados(Simulação)
   String user = "Igor Souza";
   String email = "test@teste.com.br";
